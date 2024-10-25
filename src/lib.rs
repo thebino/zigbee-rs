1320c95533c31be6d7f54af482b79ded6f820185
Patrick Willner <50421879+heat1q@users.noreply.github.com>
2024-10-26 00:16:37 +0200

    add parse from bytes trait

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,7 @@
 //! [ZigBee specification]: https://csa-iot.org/wp-content/uploads/2022/01/docs-05-3474-22-0csg-zigbee-specification-1.pdf
 //!
 #![no_std]
-pub mod nwk;
+mod spec;
+pub use spec::*;
+
+pub(crate) mod parse;
