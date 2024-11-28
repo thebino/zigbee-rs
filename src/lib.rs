417d3318f8c1e9fd9fd30da107ae33577f19eab8
Benjamin Stürmer <benjamin@stuermer.pro>
2024-11-28 07:17:11 +0100

    add test coverage to CI

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,5 @@
 //!
 //! ZigBee is a protocol stack based on the ZigBee specification 22 1.0
 //!
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-#[cfg(test)]
-mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::*;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
-    }
-}
+mod lib {}
 
