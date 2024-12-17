72223a3e43fbcd4aec55de7f677c477c975e8be4
Benjamin Stürmer <benjamin@stuermer.pro>
2024-12-17 22:07:56 +0100

    add comments

--- src/zdo/mod.rs
+++ src/zdo/mod.rs
@@ -23,18 +23,26 @@ impl ZigbeeDevice {
 
     }
 
+    /// 2.1.3.1 - Device Discovery
+    /// is the process whereby a ZigBee device can discover other ZigBee devices.
     pub fn start_device_discovery(&self) {
         match self.config.device_discovery_type {
             config::DiscoveryType::IEEE => {
                 todo!()
                 // TODO: send IEEE address request as unicast to a particular device 
+                // TODO: wait for incoming frames
             },
             config::DiscoveryType::NWK => {
                 todo!()
                 // TODO: send NWK address request as broadcast with the known IEEE address as data payload
+                // TODO: wait for incoming frames
+
             },
         }
     }
+
+    /// 2.1.3.2 - Service Discovery
+    /// is the process whereby the capabilities of a given device are discovered by other devices.
     pub fn start_service_discovery(&self) {}
 }
 
